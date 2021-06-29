ee28aa3fdfab30f9e09aa8038569bf6a976a288f
Lucas Piputto <lucaspiputto@gmail.com>
2021-06-29 14:33:43 -0300

    step 6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Filter } from './components/Filter';
 import { PersonForm } from './components/PersonForm';
 import { PersonsToShow } from './components/PersonsToShow';
+import axios from 'axios';
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchedName, setSearchedName] = useState('');
   const [selectedPersons, setSelectedPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(res => {
+        console.log(res.data);
+        setPersons(res.data);
+      });
+  }, []);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
