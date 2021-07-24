6bf1d9fb95bf738996a07a93b3db1c57f65ab2cc
Lucas Piputto <lucaspiputto@gmail.com>
2021-07-24 12:55:13 -0300

    3.4: Phonebook backend step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const persons = [
+let persons = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -41,6 +41,13 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end();
   };
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  // console.log(id, typeof id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
 });
 
 const unknownEndpoint = (req, res) => {
