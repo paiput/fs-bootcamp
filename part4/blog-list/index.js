5314667d7fe611cf2c7f6d9af9c75478c53eac8e
Lucas Piputto <lucaspiputto@gmail.com>
2021-12-04 17:38:30 -0300

    4.2 Blog list, step2
    Separate routes into their own file in controllers folder

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -4,15 +4,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
+const blogsRouter = require('./controllers/blogs');
 
 const mongoUrl = process.env.MONGODB_URI;
 
@@ -27,23 +19,7 @@ mongoose.connect(mongoUrl)
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
-});
+app.use('/api/blogs', blogsRouter);
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
