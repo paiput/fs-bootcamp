bbaec2db02da7072b84559336be6bf80abd2bbb7
Lucas Piputto <lucaspiputto@gmail.com>
2021-12-04 17:58:56 -0300

    4.2 Blog list, step2
    Create utils folder

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,27 +1,27 @@
-require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 
-const mongoUrl = process.env.MONGODB_URI;
-
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
+    logger.info('Connected to MongoDB');
   })
   .catch((error) => {
-    console.error('Failed to connect to MongoDB:', error.message);
+    logger.error('Failed to connect to MongoDB:', error.message);
   });
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 
-const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
