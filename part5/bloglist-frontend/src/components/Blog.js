014bf8f107da4f2aa8d2401e553c734d512f603d
Lucas Piputto <lucaspiputto@gmail.com>
2022-02-07 17:07:47 -0300

    5.7* Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>  
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingLeft: 2,
+    border: '1px solid',
+    marginBottom: 5
+  };
+
+  const toggleVisibility = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <p>{blog.title} <button onClick={toggleVisibility}>{showDetails ? 'hide' : 'view'}</button></p>
+      <ul style={{ display: showDetails ? 'block' : 'none' }}>
+        <li>{blog.url}</li>
+        <li>likes 0 <button>like</button></li>
+        <li>{blog.author}</li>
+      </ul>
+    </div>  
+  );
+};
 
 export default Blog;
