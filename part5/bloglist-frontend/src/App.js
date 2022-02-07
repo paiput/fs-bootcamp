258f0c68876b3a89d8dffbb44a91b9ce366fd37c
Lucas Piputto <lucaspiputto@gmail.com>
2022-02-07 16:11:31 -0300

    5.5 and 5.6: Blog list frontend, steps 5 and 6

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,12 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Blog from './components/Blog';
 import BlogForm from './components/BlogForm';
 import LoginForm from './components/LoginForm';
+import Togglable from './components/Togglable';
 import blogService from './services/blogs';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
+
+  const [errorMsg, setErrorMsg] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
+
+  const blogFormRef = useRef();
 
   useEffect(() => {
     const loggedUser = localStorage.getItem('loggedUser');
@@ -23,6 +29,24 @@ const App = () => {
     setUser(null);
   };
 
+  const addBlog = async blogObject => {
+    console.log(blogFormRef.current)
+    try {
+      blogFormRef.current.toggleVisibility();
+      const newBlog = await blogService.create(blogObject);
+      setBlogs(blogs => blogs.concat(newBlog));
+      setSuccessMsg(`a new blog: ${newBlog.title} by ${newBlog.author} added to bloglist`);
+      setTimeout(() => {
+        setSuccessMsg('');
+      }, 5000);
+    } catch(error) {
+      setErrorMsg(error.response?.data?.error || error.message);
+      setTimeout(() => {
+        setErrorMsg('');
+      }, 5000);
+    }
+  };
+
   if (user === null) {
     return (
       <div>
@@ -36,7 +60,13 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
-      <BlogForm setBlogs={setBlogs} />
+      <Togglable buttonLabel="new blog" ref={blogFormRef}>
+        <BlogForm 
+          createBlog={addBlog}
+          successMsg={successMsg}
+          errorMsg={errorMsg}
+        />
+      </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -1,13 +1,10 @@
 import { useState } from 'react';
-import blogService from '../services/blogs';
 import Notification from './Notification';
 
-const BlogForm = ({ setBlogs }) => {
+const BlogForm = (props) => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [url, setUrl] = useState('');
-  const [errorMsg, setErrorMsg] = useState('');
-  const [successMsg, setSuccessMsg] = useState('');
 
   const handleTitleChange = event => {
     setTitle(event.target.value);
@@ -21,37 +18,24 @@ const BlogForm = ({ setBlogs }) => {
     setUrl(event.target.value);
   };
 
-  const handleFormSubmit = async event => {
-    try {
-      event.preventDefault();
-      const blog = {
-        title,
-        author,
-        url
-      };
-      const newBlog = await blogService.create(blog);
-      setBlogs(prevValue => prevValue.concat(newBlog));
-      setSuccessMsg(`a new blog: ${newBlog.title} by ${newBlog.author} added to bloglist`);
-      setTitle('');
-      setAuthor('');
-      setUrl('');
-      setTimeout(() => {
-        setSuccessMsg('');
-      }, 5000);
-    } catch(error) {
-      setErrorMsg(error.response.data.error);
-      setTimeout(() => {
-        setErrorMsg('');
-      }, 5000);
-    }
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.createBlog({
+      title,
+      author,
+      url
+    });
+    setTitle('');
+    setAuthor('');
+    setUrl('');
   };
-  
+
   return (
     <div>
       <h2>create new</h2>
-      {errorMsg && <Notification type="danger" msg={errorMsg} />}
-      {successMsg && <Notification type="success" msg={successMsg} />}
-      <form onSubmit={handleFormSubmit}>
+      {props.errorMsg && <Notification type="danger" msg={props.errorMsg} />}
+      {props.successMsg && <Notification type="success" msg={props.successMsg} />}
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="title-input">title:</label>
           <input 
